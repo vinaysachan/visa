cb140f733b351c5567cc05948d8528af537b2b48
Mukesh <mukesh.fiv@gmail.com>
2016-12-12 22:51:02 +0530

    mukesh adddes front pages

---
+++ application/views/html/common/header.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>        
+        <title>Title</title> 
+        <meta name="description" content="Desc">
+        <meta name="keywords" content="Keywords"> 
+        <meta property="og:description" content="Desc" />
+        <meta property="og:site_name" content="Sitename" />
+        <meta property="og:title" content="tite" />
+        <link href="<?= base_url('public/plugins/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url('plugins/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic|Roboto+Slab:400,700|Inconsolata:400,700&subset=latin,cyrillic" rel="stylesheet" type="text/css" />
+        <link rel="icon" href="" type="image/x-icon">
+
+        <!--Themes Start-->
+
+        <link rel="stylesheet" type="text/css" href="<?= base_url('public/themes') ?>/css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="<?= base_url('public/themes') ?>/css/animate.css">
+        <link rel="stylesheet" href="<?= base_url('public/themes') ?>/css/style.css" type="text/css"/>
+        <link rel="stylesheet" href="<?= base_url('public/themes') ?>/css/menu.css" type="text/css"/>
+        <!-- the CSS for Smooth Div Scroll -->
+        <link rel="stylesheet" type="text/css" href="contact/contact.css" /> 
+        <link rel="Stylesheet" type="text/css" href="<?= base_url('public/themes') ?>/css/smoothDivScroll.css" />
+<script type="text/javascript" src="<?= base_url('public/js/jquery.min.js') ?>"></script>
+        <style type="text/css">
+            .flex-prev{background:url(<?=base_url('public/themes')?>/images/pr1.png) no-repeat !important;width:25PX !important;height:58PX !important;display:block; display:none}
+            .flex-next{background:url(<?=base_url('public/themes')?>/images/pr2.png) no-repeat !important;width:25PX !important;height:58PX !important;display:block}
+        </style>
+        <!--Themes End-->
+		 
+    </head>
+    <body>
+        
+            
+            <header>
+                <div class="top_band" style="border-top:solid 5px #FCB040;"> 
+                    <div class="wrapper" > 
+                        <!-- logo starts -->
+                        <div style="float:left;height:110px;">
+                            <a href="default.html"><img src="http://e-indianvisa.com/assets/images/logo-4.png" alt="JD Tech" class="logo" height="50" style="margin-top:20px;" ></a> 
+                        </div>
+                        <!-- logo ended -->
+                        <div class="top_m_bar" style="float:right">
+                            <!-- menu -->
+                            <div class="menu-bg">
+                                <div > 
+                                    <div>
+                                        <div id='cssmenu' style="z-index:10 !important; ">
+                                            <ul class="pad_m">
+                                                <li class='active'><a href="Default-2.html" title="Home">Home</a></li>
+
+                                                <li ><a href='Products.html' title="Products">Urgent Visa</a>
+<!--                                                    <ul>
+                                                        <li><a href="POS-Point-Of-Sale-Devices.html" title="POS (Point Of Sale) Devices">POS (Point Of Sale) Devices </a></li>
+                                                        <li><a href="mPOS-Mobile-POS-Devices.html" title="mPOS (Mobile POS) Devices">mPOS (Mobile POS) Devices</a></li>  
+                                                        <li><a href="Biometric-Devices.html" title="Biometric Devices">Biometric Devices</a></li>  
+                                                        <li><a href="CCTV-Camera-and-DVRs.html" title="CCTV Camera & DVRs">CCTV Camera & DVRs</a></li>  
+                                                        <li><a href="Solar-Power.html" title="Solar Power">Solar Power</a></li>  
+                                                        <li><a href="LED-Lighting.html" title="Solar Power">LED Lighting</a></li>  		   
+                                                    </ul>-->
+                                                </li>
+
+                                                <li ><a href='Services.html' title="Services">Apply Visa</a>
+                                                    <ul style="display:block">
+                                                        <li style="display:block" >
+                                                            <a href="IT-Infra-Management.html" title="IT Infra Management">IT Infra Management</a></li>
+                                                        <li><a href="Desktop-Applications.html" title="Desktop Applications ">Desktop Applications </a></li>
+                                                        <li><a href="Web-Applications.html" title="Web Applications">Web Applications</a></li>
+                                                        <li><a href="Mobile-Apps.html" title="Mobile Apps">Mobile Apps</a></li>
+                                                        <li><a href="Enterprise-Application-Integration.html" title="Enterprise Application Integration">Enterprise Application Integration</a></li>
+                                                        <li><a href="Staffing.html" title="Staffing">Staffing</a></li> 
+
+                                                    </ul>
+                                                </li>
+
+                                                <li ><a href='Skills-Development.html' title="Services">Terms & Condition</a>
+                                                    <ul style="display:block">
+                                                        <li style="display:block" > <a href="Internship.html" title="Internship">Internship</a></li>
+                                                        <li><a href="Project-Trainings.html" title="Project Trainings">Project Trainings </a></li>
+                                                        <li><a href="Corporate-Trainings.html" title="Corporate Trainings">Corporate Trainings</a></li> 
+
+                                                    </ul>
+                                                </li>
+                                                <li ><a href="Contact-Us.html" title="Contact Us">Documents</a></li>
+
+                                                <li ><a href='About-Us.html' title="About Us">Contact Us</a>
+                                                    <ul>
+                                                        <li><a href="About-Us.html" title="Overview">Overview </a></li>
+                                                        <li><a href="Clients-Speak.html" title="Clients Speak">Clients Speak</a></li>  
+                                                        <li><a href="Careers.html" title="Careers">Careers</a></li>   		   
+                                                    </ul>
+                                                </li>
+
+                                                 
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="clr"> </div>
+                            <!-- menu ends -->
+
+                        </div>
+                    </div>
+
+
+                    <!-- pop up -->
+
+
+
+                   
+                  
+                   
+
+
+                     
+                    <script src="js/light_box.js"></script>    </div>
+            </header>
+            <!-- header ended -->
+ 
+
+        <div class="clear"> </div>
+
+
